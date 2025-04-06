80c0f480c7f384719ab7b8fc693fe4c9e198e29d
Nickosss97435 <nickosssanonymous@gmail.com>
2025-04-06 12:04:48 +0400

    mise à jour vercel.json

--- vite.config.js
+++ vite.config.js
@@ -1,20 +1,18 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
-// https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [
-    react({
-      include: /\.(mdx|js|jsx|ts|tsx)$/,
-    }),
-  ],
+  plugins: [react()],
   server: {
-    host: "0.0.0.0", // Écoute sur toutes les interfaces réseau
-    port: 5173,      // Port par défaut
+    host: "0.0.0.0",
+    port: 5173,
   },
   resolve: {
-    alias: {
-      // Autes alias
-    },
+    alias: {},
+  },
+  base: '/', // Assurez-vous que base est défini
+  build: {
+    outDir: 'dist', // Assurez-vous que le outDir est correct
   },
 });
+
